574f70261f08da1c8825e75404e2ffff17a26c8d
weiaquarius <weiaquarius17@gmail.com>
2020-04-28 19:36:14 +0800

    change file location

--- exam01/LCD.cpp
+++ exam01/LCD.cpp
@@ -11,9 +11,8 @@ int main()
     // uLCD.text_width(4); //4X size text
     // uLCD.text_height(4);
     uLCD.color(RED);
-
     uLCD.line(0, 0, 60, 0, 0xFF0000);
     uLCD.line(0, 0, 0, 60, 0xFF0000);
     uLCD.line(60, 60, 0, 60, 0xFF0000);
-    // uLCD.line(0, 60, 60, 60, 0xFF0000);
+    uLCD.line(60, 60, 60, 0, 0xFF0000);
 }
