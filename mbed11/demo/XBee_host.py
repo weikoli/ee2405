b2344e590da18d2ece4b6eedff626ae9b7aac477
weiaquarius <weiaquarius17@gmail.com>
2020-06-02 17:32:27 +0800

    update

---
+++ mbed11/demo/XBee_host.py
@@ -0,0 +1,77 @@
+import serial
+import time
+
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x158\r\n".encode())
+char = s.read(3)
+print("Set MY 0x158.")
+print(char.decode())
+
+s.write("ATDL  0x258\r\n".encode())
+char = s.read(3)
+print("Set DL  0x258.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+
+print("start sending RPC")
+while True:
+    # send RPC to remote
+    
+    s.write("/getAddr/run\r".encode())
+    time.sleep(1)
+
+s.close()
+# import serial
+# import time
+
+# # XBee setting
+# serdev = '/dev/ttyUSB0'
+# s = serial.Serial(serdev, 9600)
+
+# s.write("+++".encode())
+# s.write("ATMY 0x158\r\n".encode())
+# s.write("ATDL 0x258\r\n".encode())
+# s.write("ATID 0x1\r\n".encode())
+# s.write("ATWR\r\n".encode())
+# s.write("ATMY\r\n".encode())
+# s.write("ATDL\r\n".encode())
+# s.write("ATCN\r\n".encode())
+
+# print("start sending RPC")
+
+# while True:
+#     s.write("/getAcc/run\r".encode())
+#     time.sleep(1)    
+# s.close()
